43732d761b2379ae9c4e188fe19ee97e1399a6e4
mohammad <mohammadgholeh@gmail.com>
2022-02-12 02:14:33 +0200

    defualt route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return redirect('todo');
+});
 Route::resource('todo', TodoController::class)->only([
     'index','create', 'store'
 ]);
